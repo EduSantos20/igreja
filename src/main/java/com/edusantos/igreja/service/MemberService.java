a5906d7043fbc363fae3ea87233d44480dbc3b56
EduSantos20 <eduardosantosgt18@gmail.com>
2026-01-14 00:26:24 -0300

    creat the MemberService

---
+++ src/main/java/com/edusantos/igreja/service/MemberService.java
@@ -0,0 +1,71 @@
+package com.edusantos.igreja.service;
+
+import com.edusantos.igreja.entities.Church;
+import com.edusantos.igreja.entities.Member;
+import com.edusantos.igreja.repository.ChurchRepository;
+import com.edusantos.igreja.repository.MemberRepository;
+import com.edusantos.igreja.service.execeptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    private final ChurchRepository churchRepository;
+
+    public MemberService(MemberRepository memberRepository, ChurchRepository churchRepository) {
+        this.memberRepository = memberRepository;
+        this.churchRepository = churchRepository;
+    }
+
+    public List<Member> findAll(){
+        return memberRepository.findAll();
+    }
+
+    public Member findById(Long id){
+        Optional<Member> objetc = memberRepository.findById(id);
+        return objetc.get();
+    }
+
+    public Member createMember(Long churchId, Member member){
+        try{
+            Church churchEntity = churchRepository.findById(churchId).get();
+
+            member.setChurch(churchEntity);
+
+            return memberRepository.save(member);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(churchId);
+        }
+    }
+
+    public Member updateMember(Long id, Long churchId, Member object){
+        try {
+            Church churchEntity = churchRepository.findById(churchId).get();
+
+            Member entity = memberRepository.getReferenceById(id);
+
+            entity.setChurch(churchEntity);
+
+            entity.setStatus(object.getStatus());
+            entity.setRole(object.getRole());
+            entity.setBaptismDate(object.getBaptismDate());
+            entity.setAdmission(object.getAdmission());
+            entity.setName(object.getName());
+            entity.setGender(object.getGender());
+            entity.setBirthdate(object.getBirthdate());
+            entity.setAddress(object.getAddress());
+            entity.setState(object.getState());
+            entity.setOccupation(object.getOccupation());
+
+            return memberRepository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+}
