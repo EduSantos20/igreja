4f800d25209a9e1f6957ce5bcae210e4f35131dd
EduSantos20 <eduardosantosgt18@gmail.com>
2026-01-14 22:22:37 -0300

    creat the services, FinancialService, OccasionService, PrayService, StudyService

--- src/main/java/com/edusantos/igreja/service/FinancialService.java
+++ src/main/java/com/edusantos/igreja/service/FinancialService.java
@@ -36,5 +36,19 @@ public class FinancialService {
         Financial existingFinancial = financialRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(id));
 
+        existingFinancial.setType(financial.getType());
+        existingFinancial.setAmount(financial.getAmount());
+        existingFinancial.setTheDate(financial.getTheDate());
+        existingFinancial.setPaymentMethod(financial.getPaymentMethod());
+        existingFinancial.setDescription(financial.getDescription());
+
+        return financialRepository.save(existingFinancial);
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Financial financial = financialRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+        financialRepository.delete(financial);
     }
 }
